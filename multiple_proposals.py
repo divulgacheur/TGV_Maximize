ee8e8c1398d2c464ecfd31a9c5d8b95c71ed1349
Théo <github@theopeltier.me>
2022-01-07 07:39:03 +0100

    :rotating_light: refactor(multiple_proposals): refactor based on Pylint report

--- multiple_proposals.py
+++ multiple_proposals.py
@@ -1,11 +1,15 @@
-from BColors import BColors
 from typing import TYPE_CHECKING
+from BColors import BColors
 
 if TYPE_CHECKING:
-    from Proposal import Proposal
+    from proposal import Proposal
 
 
 class MultipleProposals:
+    """
+    Class that represents a multiple proposal.
+    """
+
     def __init__(self, *proposals: 'Proposal'):
         self.proposals = proposals
 
@@ -31,7 +35,9 @@ class MultipleProposals:
             f'({first.departure_date.strftime("%H:%M")}) → '
             f'{first.arrival_station.name} ({first.arrival_date.strftime("%H:%M")})',
             f'{first.transporter} {first.vehicle_number}' if long else '',
-            f'⭾ {second.departure_station.name} ({second.departure_date.strftime("%H:%M")})' if second.departure_station.code != first.arrival_station.code else f'⏲  ({second.departure_date.strftime("%H:%M")})',
+            f'⭾ {second.departure_station.name} ({second.departure_date.strftime("%H:%M")})'
+            if second.departure_station.code != first.arrival_station.code
+            else f'⏲  ({second.departure_date.strftime("%H:%M")})',
             # If connection stations are different, i.e. Nimes <-> Nimes Pont du Gard, display the name of two connection stations
             # Else, display only the station name once
             f'→ {second.arrival_station.name} ({second.arrival_date.strftime("%H:%M")})',
@@ -41,15 +47,15 @@ class MultipleProposals:
         )
 
     @staticmethod
-    def display(first_segment, second_segment, berth_only: bool = False, long: bool = False) -> None:
+    def display(segment1, segment2, berth_only: bool = False, long: bool = False) -> None:
         """
-        :param first_segment: list of proposals for the first segment
-        :param second_segment: list of proposals for the second segment
+        :param segment1: list of proposals for the first segment
+        :param segment2: list of proposals for the second segment
         :param berth_only: enable printing only the Intercites de Nuit proposals with a berth
         :param long: enable printing of detailed proposals, including the transporter and the vehicle number
         :return:
         """
-        for first_proposal in first_segment:
-            for second_proposal in second_segment:
+        for first_proposal in segment1:
+            for second_proposal in segment2:
                 if second_proposal.departure_date > first_proposal.arrival_date:
                     MultipleProposals(first_proposal, second_proposal).print(berth_only=berth_only, long=long)
