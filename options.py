844cd099a31ef5f5c717530ad3d0cdb9526522de
Théo <github@theopeltier.me>
2022-01-08 14:21:00 +0100

    :rotating_light: refactor(options): refactor based on Pylint report

---
+++ options.py
@@ -0,0 +1,32 @@
+class SearchOptions:
+    """
+    Class to define options for searching
+    """
+    via: str
+    berth_only: bool = False
+    long: bool = False
+    direct_only: bool = False
+    max_duration: int
+
+    def __init__(self, via=None, max_duration=None, berth_only=False, long=False,
+                 direct_only=False, verbosity=False, quiet=False) -> None:
+        self.via = via
+        self.berth_only = berth_only
+        self.long = long
+        self.direct_only = direct_only
+        self.max_duration = max_duration
+        self.verbosity = verbosity
+        self.quiet = quiet
+
+
+class PromptOptions:
+    """
+    Class to define options for prompting
+    """
+
+    verbosity: bool
+    quiet: bool
+
+    def __init__(self, verbosity, quiet):
+        self.verbosity = verbosity
+        self.quiet = quiet
