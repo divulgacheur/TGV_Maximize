39232d07f7c280ba11c1ef34f55fdd173e6fd4c3
Théo <github@theopeltier.me>
2022-01-11 16:37:17 +0100

    :rotating_light: refactor(main): refactor based on Pylint report

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from sys import exit as sys_exit
 from argcomplete import autocomplete
 from pyhafas import HafasClient
 from pyhafas.profile import DBProfile
-from requests import get, session
+from requests import session
 
 from bcolors import BColors
 from direct_destination import DirectDestination
@@ -63,40 +63,47 @@ def get_available_seats(dep_station: str, arr_station: str, day: datetime,
     return Proposal.remove_duplicates(all_proposals, opts.verbosity) if all_proposals else []
 
 
-def get_indirect_proposals(departure, arrival, departure_direct_destinations, arrival_direct_destinations, day, opts):
-    print(f"Let's try to split the journey from {departure.name} to {arrival.name}", end=' : ')
+def display_indirect_proposals(dpt_direct_dest, arr_direct_dest, day, opts):
+    """
+    Display indirect train proposals for a given day
+    :param dpt: station of departure
+    :param arr: station of arrival
+    :param dpt_direct_dest: direct destinations of departure
+    :param arr_direct_dest: direct destinations of arrival
+    :param day: date of departure
+    :param opts: search options
+    """
+
     if not opts.via:
-        intermediate_stations = departure_direct_destinations.get_common_stations(
-            arrival_direct_destinations)   + [PARIS]
+        intermediate_stations = dpt_direct_dest.get_common_stations(
+            arr_direct_dest) + [PARIS]
     else:
-        via_code, formal_via_name = Station.name_to_code(Station(opts.via))
-        intermediate_stations = [
-            {'station': Station(opts.via, code=via_code,
-                                formal_name=formal_via_name,
-                                identifier=client.locations(opts.via)[0].__dict__['id'])}]
+        via = Station(opts.via)
+        via.get_code()
+        via.get_identifier()
+        intermediate_stations = [{'station': via}]
 
     for intermediate_station in intermediate_stations:
-
         if intermediate_station['station'].is_in_france():
             # check for segments between station located in France only
             if not opts.quiet:
                 print(f"\nVia {intermediate_station['station'].name}")
 
-            farther_station = Station.get_farther_station(
-                departure_direct_destinations,
-                arrival_direct_destinations,
+            farther_station = Station.get_farther(
+                dpt_direct_dest,
+                arr_direct_dest,
                 intermediate_station)
 
+            segments = [{'dpt': dpt_direct_dest.station, 'arr': intermediate_station['station']},
+                        {'dpt': intermediate_station['station'], 'arr': arr_direct_dest.station}]
+
             if farther_station == intermediate_station:
-                segments = {0: {'departure': departure, 'arrival': intermediate_station['station'],
-                                1: {'departure': intermediate_station['station'], 'arrival': arrival}}}
-            else:
-                segments = {1: {'departure': intermediate_station['station'], 'arrival': arrival},
-                            0: {'departure': departure, 'arrival': intermediate_station['station'], }}
+                segments.reverse()
+
             results = {}
-            for index, segment in segments.items():
-                result = get_available_seats(segment['departure'].name_to_code()[0],
-                                             segment['arrival'].name_to_code()[0],
+            for index, segment in enumerate(segments):
+                result = get_available_seats(segment['dpt'].name_to_code()[0],
+                                             segment['arr'].name_to_code()[0],
                                              day,
                                              opts=SearchOptions(
                                                  verbosity=opts.verbosity,
@@ -116,7 +123,7 @@ def get_indirect_proposals(departure, arrival, departure_direct_destinations, ar
                 # (most demanded than the shortest and potentially limiting factor)
                 # Exemple : For Beziers-Paris (~4h) via Nimes, we first search for
                 # the journey from Nimes to Paris (~3h), then for the journey
-                # from Beziers-Nimes (~1h), because longer segment is rare
+                # from Beziers-Nimes (~1h), because longer segment is rarer
 
                 if len(results) > 1:  # Display results if more than one segment found
                     MultipleProposals.display(results[0], results[1], opts.berth_only, opts.long)
@@ -124,48 +131,41 @@ def get_indirect_proposals(departure, arrival, departure_direct_destinations, ar
 
 def total_search(dpt_name: str, arr_name: str, days: int, days_delta: int, opts: SearchOptions):
     """
-    :param dpt_name:
-    :param arr_name:
-    :param days:
-    :param days_delta:
-    :param opts:
-    :return:
+    :param dpt_name: name of departure station
+    :param arr_name: name of arrival station
+    :param days: number of days to search
+    :param days_delta: number of days to search from today
+    :param opts: search options
     """
     date = datetime.now().replace(hour=0, minute=0) + timedelta(days=days_delta)
 
-    departure_code, formal_departure_name = Station.name_to_code(Station(dpt_name))
-    arrival_code, formal_arrival_name = Station.name_to_code(Station(arr_name))
+    departure = Station(dpt_name)
+    departure.get_code()
+    departure.get_identifier()
+    dpt_direct_dest = DirectDestination.get(departure)
 
-    departure = Station(dpt_name, code=departure_code, formal_name=formal_departure_name,
-                        identifier=client.locations(dpt_name)[0].__dict__['id'])
-    arrival = Station(arr_name, code=arrival_code, formal_name=formal_arrival_name,
-                      identifier=client.locations(arr_name)[0].__dict__['id'])
-
-    arrival_direct_destinations = DirectDestination(arrival, get('https://api.direkt.bahn.guru/' + arrival.identifier))
-    departure_direct_destinations = DirectDestination(departure,
-                                                      get('https://api.direkt.bahn.guru/' + departure.identifier))
+    arrival = Station(arr_name)
+    arrival.get_code()
+    arrival.get_identifier()
+    arr_direct_dest = DirectDestination.get(arrival)
 
     for day_counter in range(days):
         day = date + timedelta(days=day_counter)
         print(day.strftime("%c"))
 
-        print(f"Direct journey from {formal_departure_name} to {formal_arrival_name}")
-        direct_proposals = get_available_seats(departure_code, arrival_code, day,
+        print(f"Direct journey from {departure.formal_name} to {arrival.formal_name}")
+        direct_proposals = get_available_seats(departure.code, arrival.code, day,
                                                SearchOptions(opts.verbosity, opts.max_duration))
         Proposal.display(direct_proposals, opts.berth_only, opts.long)
 
         if not opts.direct_only:
-            get_indirect_proposals(departure, arrival,
-                                   departure_direct_destinations,
-                                   arrival_direct_destinations,
-                                   day,
-                                   opts)
+            print(f"Let's split the journey from {departure.name} to {arrival.name}", end=' : ')
+            display_indirect_proposals(dpt_direct_dest, arr_direct_dest, day, opts)
 
 
 def main():
     """
     Main function
-    :return:
     """
     parser = ArgumentParser()
     parser.add_argument("stations", metavar="station", help="Station names", nargs=2)
@@ -173,13 +173,15 @@ def main():
     parser.add_argument("-p", "--period", help="Number of days to search", type=int, default=1)
     parser.add_argument("-d", "--direct-only", help="Print direct proposals only",
                         action="store_true")
-    parser.add_argument("-b", "--berth-only", help="Print berth only for Intercites de Nuit proposals",
+    parser.add_argument("-b", "--berth-only", help="Print berth only "
+                                                   "for Intercites de Nuit proposals",
                         action="store_true")
     parser.add_argument("--via", type=str, help="Force connection station with specified name")
-    parser.add_argument("-l", "--long",
-                        help="Add details for prompted proposals, including transporter and vehicle number",
+    parser.add_argument("-l", "--long", help="Add details for prompted proposals,"
+                                             " including transporter and vehicle number",
                         action="store_true")
-    parser.add_argument("--max-duration", type=int, help="Maximum duration of a journey", default=600)
+    parser.add_argument("--max-duration", type=int, help="Maximum duration of a journey",
+                        default=600)
     parser.add_argument("-q", "--quiet", help="Only show results", action="store_true")
     parser.add_argument("-v", "--verbosity", action="store_true", help="Verbosity")
     autocomplete(parser)
