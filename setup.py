3cc62751674622954d0fe26987a87da7151ee322
Théo <github@theopeltier.me>
2022-01-13 02:53:44 +0100

    :pushpin: feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='TGV_Maximize',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Divulgacheur',
+      author_email='github@theopeltier.me',
+      )
