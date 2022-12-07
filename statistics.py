e88c24f77ff165759ce5f98860ad5639bf4bc464
Théo <github@theopeltier.me>
2022-12-07 16:38:47 +0100

    :boom: begin stats support for SNCF connect

--- statistics.py
+++ statistics.py
@@ -1,5 +1,5 @@
 from sys import exit as sys_exit
-from requests import get
+from requests import post
 
 from config import Config
 
@@ -13,22 +13,19 @@ class Statistics:
 
     # noinspection SpellCheckingInspection
     def __init__(self):
+
         headers = {
-            'authority': 'www.oui.sncf',
-            'cache-control': 'max-age=0',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-gpc': '1',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-dest': 'document',
-            'accept-language': 'fr-FR,fr;q=0.9',
-            'Cookie': Config.OUISNCF_STATS_COOKIE
-        }
-        response = get(
-            'https://www.oui.sncf/api/gtw/aftersale/prd/vsa/api/v2/orders/oui-account?ascSort=false&localeParam=fr_FR&pageNumber=1&pageResultsCount=200&statusFilter=PASSED',
-            headers=headers)
+        'authority': 'www.sncf-connect.com',
+        'accept': 'application/json, text/plain, */*',
+        'accept-language': 'fr-FR,fr;q=0.9',
+        'cache-control': 'no-cache',
+
+    }
+
+
+        response = post(
+            'https://www.sncf-connect.com/bff/api/v1/trips',
+            headers=headers, json = {}        )
         if response.status_code == 200:
             self.data = response.json()
         else:
@@ -38,21 +35,23 @@ class Statistics:
         """
         Analyze the order history from oui.sncf account
         """
-        for order in self.data['orders']:
-            if 'trainFolders' in order:  # Order must contain train, exclude discount cards
-                self.amount += order['trainFolders'][0]['amount']  # count total amount spend
-                for segment in order['trainFolders'][0]['travels'][0]['segments']:
-                    # for each segment in the travel
-                    for station in ('origin', 'destination'):
-                        if segment[station]['stationName'] in self.frequented_stations:
-                            self.frequented_stations[segment[station]['stationName']] += 1
-                            # if key exists increment value, else set value to 1
-                        else:
-                            self.frequented_stations[segment[station]['stationName']] = 1
+        if 'response' in self.data and 'cancelledTrips' in self.data['response']:
+            for trip in self.data['response']['cancelledTrips']:
+                print(trip)
 
-        print(f'Total amount: {str(self.amount)} €')
-        favorites_stations = sorted(self.frequented_stations.items(), key=lambda stations: stations[1], reverse=True)
-        print(f'Favorites stations: {favorites_stations[:5]}')
+    #         self.amount += self.data['passedTrips'][0]['amount']  # count total amount spend
+    #         for segment in order['trainFolders'][0]['travels'][0]['segments']:
+    #             # for each segment in the travel
+    #             for station in ('origin', 'destination'):
+    #                 if segment[station]['stationName'] in self.frequented_stations:
+    #                     self.frequented_stations[segment[station]['stationName']] += 1
+    #                     # if key exists increment value, else set value to 1
+    #                 else:
+    #                     self.frequented_stations[segment[station]['stationName']] = 1
+    #
+    # print(f'Total amount: {str(self.amount)} €')
+    # favorites_stations = sorted(self.frequented_stations.items(), key=lambda stations: stations[1], reverse=True)
+    # print(f'Favorites stations: {favorites_stations[:5]}')
 
 
 s = Statistics()
